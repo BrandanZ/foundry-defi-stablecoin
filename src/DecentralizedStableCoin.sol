8671780a984b26ca85b4c5bbe97432cea053998c
brandon <branthebuilder321@gmail.com>
2023-11-07 12:10:54 -0500

    Initial commit

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title DecentralizedStableCoin
+ * @author Brandon Stark
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Collateralized
+ * Relative Stability: Pegged to USD
+ * 
+ * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system. 
+ * 
+ */
+
+contract DecentralizedStableCoin {
+
+}
