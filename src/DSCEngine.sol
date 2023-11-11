244d2fbfe36f2f1afdc14487bf9d0fc93a071c97
brandon <branthebuilder321@gmail.com>
2023-11-11 16:23:32 -0500

    Add external view/pure functions to get data

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -365,4 +365,36 @@ contract DSCEngine is ReentrancyGuard {
     function getPrecision() external pure returns (uint256) {
         return PRECISION;
     }
+
+    function getLiquidationThreshold() external pure returns (uint256) {
+        return LIQUIDATION_THRESHOLD;
+    }
+
+    function getLiquidationBonus() external pure returns (uint256) {
+        return LIQUIDATION_BONUS;
+    }
+
+    function getLiquidationPrecision() external pure returns (uint256) {
+        return LIQUIDATION_PRECISION;
+    }
+
+    function getMinHealthFactor() external pure returns (uint256) {
+        return MIN_HEALTH_FACTOR;
+    }
+
+    function getCollateralTokens() external view returns (address[] memory) {
+        return s_collateralTokens;
+    }
+
+    function getDsc() external view returns (address) {
+        return address(i_dsc);
+    }
+
+    function getCollateralTokenPriceFeed(address token) external view returns (address) {
+        return s_priceFeeds[token];
+    }
+
+    function getHealthFactor(address user) external view returns (uint256) {
+        return _healthFactor(user);
+    }
 }
